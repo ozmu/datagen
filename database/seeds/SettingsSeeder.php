65d68d5cbcff939ce3d337da2ea6fd9daf8aec32
Muhammet Öztürk <muhammet@ozturk.com>
2020-02-14 18:03:28 +0300

    Vue layout updates and settings seeder created

---
+++ database/seeds/SettingsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Setting;
+
+class SettingsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Setting::create(['key' => 'coin_factor', 'value' => '0.30']);
+    }
+}
